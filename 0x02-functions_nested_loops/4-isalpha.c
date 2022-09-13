fa736429767b2ca88b125a4c27367079ca7928d2
slakovic <aladefm.o.a@gmail.com>
2022-09-14 00:55:16 +0100

    Task 4.Isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,9 +11,11 @@
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-	    c >= 65 && c >= 90)
-		return (1);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else 
 	return (0);
 }
 
