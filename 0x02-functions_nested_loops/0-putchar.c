b527f3db1a1180293ed42b03ebef760aa03795df
slakovic <aladefm.o.a@gmail.com>
2022-09-13 14:59:22 +0100

    Task 0.Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <main.h>
+
+/**
+ * main - print putchar followed by a newline with putchar prototype
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char str{} = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
+}
