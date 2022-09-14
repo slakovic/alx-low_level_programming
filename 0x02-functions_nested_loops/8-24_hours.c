7a5f226edcca2122c9495edd7e57eb8b7f2ae39d
slakovic <aladefm.o.a@gmail.com>
2022-09-14 01:37:47 +0100

    Task 8.24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,9 @@ void jack_bauer(void)
 {
 	int min, hr;
 	
-	for (hr = '0' hr <= 23; ++hr)
+	for (hr = 0 hr <= 23; ++hr)
 	{
-		for (min = '0'; min <= 59; ++min)
+		for (min = 0; min <= 59; ++min)
 		{
 			_putchar((hr / 10) + 48);
 			_putchar((hr % 10) + 48);
