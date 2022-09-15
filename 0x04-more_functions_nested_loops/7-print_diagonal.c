e376f1d7124a1b10e5ce73494a9c540c368702e7
slakovic <aladefm.o.a@gmail.com>
2022-09-15 22:29:29 +0100

    Task 7.print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,7 +15,7 @@ void print_diagonal(int n)
 		_putchar('\n');
 	else
 	{
-		for (postn = 1; postn <= n; ++postn)
+		for (postn = 1; postn <= n; postn++)
 		{
 			for (space = 1; space <= postn; ++space)
 				_putchar(' ');
