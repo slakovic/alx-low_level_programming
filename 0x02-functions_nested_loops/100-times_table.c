447bf6317cbe883bf07239f418319123b97e62f6
slakovic <aladefm.o.a@gmail.com>
2022-09-17 00:51:00 +0100

    Times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,51 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * print_times_table - prints time table of n
+ * print_times_table - Prints a multiplication table up to param
+ * @n: The number to be treated
  *
- * @n: takes number input
-*/
-
+ * Return: Number matrix
+ */
 void print_times_table(int n)
-	
-	int row, int coluwn, int product;
+{
+	int x, y, z;
 
-	if (n > 0 && n <= 15)
+	if (n >= 0 && n <= 14)
 	{
-		for (row = 0; num <= n; column++)
+		for (x = 0; x <= n; x++)
 		{
-			for (column = 0; column <= n; column++)
+			for (y = 0; y <= n; y++)
 			{
-				prodcut = row * colum;
-
-				if (colum == 0)
-					_putchar(0);
-				else if (product < 10)
+				z = x * y;
+				if (z > 99)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(product % 10 + '0');
-				}	
-				else if (product >= 10 && product < 100)
-				{	
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
+					_putchar(32);
+					_putchar((z / 100) + '0');
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
 				}
-				else if (product > 99 && product < 1000)
+				else if (z > 9)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar((product / 10) % 10 + '0');
-					_putchar(product % 10 + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(((z / 10) % 10) + '0');
+					_putchar((z % 10) + '0');
+				}
+				else
+				{
+					if (y != 0)
+					{
+						_putchar(',');
+						_putchar(32);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(z + '0');
 				}
 			}
 			_putchar('\n');
 		}
 	}
-
+}
