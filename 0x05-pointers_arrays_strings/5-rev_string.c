14e9e59370ae1368dfbdbeab2eeb3f4ec63f0221
slakovic <aladefm.o.a@gmail.com>
2022-09-19 23:10:41 +0100

    Tasl 5. Reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,8 +14,9 @@ void rev_string(char *s)
 	char ch;
 
 	/*find string length without null char*/
-	for (l = 0; s[l] != '\n'; ++l)
+	for (l = 0; s[l] != '\0'; ++l)
 		;
+
 	/*swap the string by looping to half the string*/
 	for (i = 0; i < l / 2; ++i)
 	{
