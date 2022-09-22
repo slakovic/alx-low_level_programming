2b16be28412631a4113d45326d1f518bcb157a9e
slakovic <aladefm.o.a@gmail.com>
2022-09-22 10:48:07 +0100

    Task 8.Rot13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,13 @@
+#ifndef Main_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * void prototypes()
+ * int prototypes()
+ * char prototypes()
+*/
+char *add_strings(char *1, char *n2, char *r, int size_r);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -10,3 +20,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
+
+#endif /*MAIN_H*/
