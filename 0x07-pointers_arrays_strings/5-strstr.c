a8715de7c9a7705f4139f8a57bad12bea2e8268f
slakovic <aladefm.o.a@gmail.com>
2022-09-26 16:03:03 +0100

    Task 5.Strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,8 +2,7 @@
  * _strstr - function that locates a substring
  *
  * @haystack: input string to search for matching substrings
- * @needle: subtring to search for 
- *
+ * @needle: subtring to search for
  * Return: pointer to the beginning located substring or NULL if not found
 */
 
