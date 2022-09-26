d16b4a892603c007bc69c64e8227e1cd51d15dad
slakovic <aladefm.o.a@gmail.com>
2022-09-26 12:43:09 +0100

    Task 2.Strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+/**
+ * _strch - A function that locates a character in a string
+ *
+ * @s: pointer to our string array input
+ * @c: character to locate from the input array
+ *
+ * Return: first occurence of character or null if not found
+*/
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	/**
+	 * if c is '\0', you should return
+	 * the pointer to the '\0' of the string s
+	*/
+	if (*s == c)
+		return (s);
+	/*return null if not found*/
+	return ('\0');
+}
