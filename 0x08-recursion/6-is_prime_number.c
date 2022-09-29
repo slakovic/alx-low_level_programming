61508b9df5e1480363dd4118cfb68b9bed5f7a23
slakovic <aladefm.o.a@gmail.com>
2022-09-29 08:03:08 +0100

    Task 6.Prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,28 +3,25 @@
 int prime_number(int n, int i);
 
 /**
- * is_prime_number - Function of if an integer is a prime number or not
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
  *
- * @n: evaluate number
- *
- * Return: if n is prime number 1, 0 if otherwise
-*/
+ * Return: 1 if n is a prime number, 0 if not
+ */
+int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (prime_number(n, n - 1);
+	return (prime_number(n, n - 1));
 }
 
 /**
  * prime_number - calculates if a number is prime recursively
- *
  * @n: number to evaluate
- *
  * @i: iterator
  *
- * Return: if n is prime 1, 0 if otherwise
-*/
-
+ * Return: 1 if n is prime, 0 if not
+ */
 int prime_number(int n, int i)
 {
 	if (i == 1)
@@ -32,10 +29,10 @@ int prime_number(int n, int i)
 		return (1);
 	}
 	else if (n % i == 0)
-	}
+	{
 		return (0);
-
-else
+	}
+	else
 	{
 		return (prime_number(n, i - 1));
 	}
