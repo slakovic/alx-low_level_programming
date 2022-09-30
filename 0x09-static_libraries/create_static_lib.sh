9c9156af8d27f4a173e635b9bdc6ac69f7510712
slakovic <aladefm.o.a@gmail.com>
2022-09-30 14:53:41 +0100

    Task 1.Create static library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# a script that creates a static library
-# called liball.a from all the .c files
-# that are in the current directory
-
-gcc -c *.c
+gcc -Wall -Wextra -Werror -pedantic -c *.c
 ar rc liball.a *.o
-echo y | rm -I *.o
