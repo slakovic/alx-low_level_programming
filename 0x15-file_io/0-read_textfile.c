62d95177607a08ee6053648390c2def7bc433dbb
slakovic <aladefm.o.a@gmail.com>
2022-10-31 21:07:52 +0100

    Task 0.Tread lightly

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,41 +1,48 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to standard output
- * @filename: name of the file
- * @letters: number of letters
- * Return: if success return number of letter, else return 0
- */
-
+ * read_textfile - a function that reads a text file and prints it
+ *                to POSIX standard output.
+ *
+ * @filename: is the file to read
+ * @letters: number of letters to read and print from file
+ *
+ * Return: 0 if it fails or actual number of letters it could
+ *         read and print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int file;
+	ssize_t read_check, wcount;
 	char *buffer;
-	ssize_t bytes_read, bytes_written;
 
-	if (filename == NULL)
+	if (filename == NULL) /*check if file is present*/
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	file = open(filename, O_RDONLY); /*open file*/
+
+	if (file == -1)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters + 1);
+	/*get the size of buffer from number of letters*/
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	read_check = read(file, buffer, letters); /*read file*/
+	if (read_check == -1) /*check if read failed*/
 		return (0);
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-		return (0);
-
-	buffer[letters + 1] = '\0';
-	close(fd);
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1)
+	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
+	if (wcount == -1 || read_check != wcount) /*check if write failed*/
 		return (0);
 
 	free(buffer);
 
-	return (bytes_read);
+	close(file); /*close file*/
+
+	return (wcount);
 }
