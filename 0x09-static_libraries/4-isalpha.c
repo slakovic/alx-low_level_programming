848edd37c6581e90e7c103eca01b1ca19cfa6853
slakovic <aladefm.o.a@gmail.com>
2023-03-02 13:10:59 +0100

    repush static library

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - checks for alphabetic character
+*@c: takes in a character
+*
+*Return: 1 if c is letter, lowercase or uppercase otherwise 0
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
+}
